cd6469e2c1a606a1870aab27af0b11e795000fe3
Liran <5064607+ushpizin@users.noreply.github.com>
2023-04-22 14:29:10 +0300

    Following schmend_lab

--- postprocessing.py
+++ postprocessing.py
@@ -10,7 +10,7 @@ from tiktokapipy.async_api import AsyncTikTokAPI
 # ghPagesURL = "https://conoro.github.io/tiktok-rss-flat/"
 
 # Custom Domain
-ghPagesURL = "https://tiktokrss.conoroneill.com/"
+ghPagesURL = "https://ushpizin.github.io/tiktok-rss-flat/"
 
 maxItems = 5
 
